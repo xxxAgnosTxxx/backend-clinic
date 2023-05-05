3863c1f2b231e3acd6c78f4a626f7ca4f7a3d70c
xxxAgnosTxxx <prochorov12@bk.ru>
2023-05-05 12:04:49 +0400

    del

--- src/main/java/com/prokhorov/clinic/repository/CardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.prokhorov.clinic.repository;
-
-import com.prokhorov.clinic.entity.Card;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardRepository extends JpaRepository<Card, Long> {
-}
