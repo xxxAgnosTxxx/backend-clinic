aa4bd7998dbbfca3e485d38e3db3efed63238c29
xxxAgnosTxxx <prochorov12@bk.ru>
2023-05-07 13:01:05 +0400

    delete unused entity

--- src/main/java/com/prokhorov/clinic/repository/ReceptionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.prokhorov.clinic.repository;
-
-import com.prokhorov.clinic.entity.Reception;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReceptionRepository extends JpaRepository<Reception, Long> {
-}
